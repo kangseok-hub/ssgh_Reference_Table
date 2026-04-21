426180d53f0cd21d10d1494f57fb639177f133db
kangseok-hub <sophia15@ssgh.hs.kr>
2026-04-21 13:23:14 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import path from 'path';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+  base: '/ssgh_Reference_Table/',  // ← 이 줄 추가
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
